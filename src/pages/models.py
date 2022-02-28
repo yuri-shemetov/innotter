a128db6b3d7dff1d0c8a1dbe8567ab646c040626
Yuri Shemetov <yuri-sh@tut.by>
2022-02-28 19:24:08 +0300

    v.1.1 Test for Users, models for pages & tags

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
+from tags.models import Tag
 
-# Create your models here.
+class Page(models.Model):
+    owner = models.ForeignKey(
+        'users.User', 
+        on_delete=models.CASCADE, 
+        related_name='pages'
+    )
+    name = models.CharField(
+        max_length=80
+    )
+    uuid = models.CharField(
+        max_length=30, 
+        unique=True
+    )
+    description = models.TextField()
+    tags = GenericRelation(Tag)
+    image = models.URLField(
+        null=True, 
+        blank=True
+    )
+    is_private = models.BooleanField(
+        default=False
+    )
+    followers = models.ManyToManyField(
+        'users.User', 
+        related_name='follows'
+    )
+    follow_requests = models.ManyToManyField(
+        'users.User', 
+        related_name='requests'
+    )
+    unblock_date = models.DateTimeField(
+        null=True, 
+        blank=True
+    )
--- src/tags/models.py
+++ src/tags/models.py
@@ -1,3 +1,23 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    owner = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='tags'
+    )
+    name = models.CharField(
+        max_length=30, 
+        unique=True
+    )
+    content_type = models.ForeignKey(
+        ContentType, 
+        on_delete=models.CASCADE
+    )
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey(
+        'content_type', 
+        'object_id'
+    )
