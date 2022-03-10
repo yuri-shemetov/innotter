e189a0bb422d9162bf0b8c8dda285357db1d6862
Yuri Shemetov <yuri-sh@tut.by>
2022-03-10 17:02:50 +0300

    Added AWS S3, Search for Users & Pages. Corrected view Posts

--- src/posts/models.py
+++ src/posts/models.py
@@ -8,5 +8,10 @@ class Post(models.Model):
        related_name='posts'
     )
     content = models.CharField(max_length=180)
+    reply_to = models.ForeignKey(
+       'posts.Post',
+       on_delete=models.SET_NULL,
+       null=True,
+       related_name='replies')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
